a654316bfda9b3cf1214db18bb6d536b228c0f3d
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-01-29 23:06:08 +0530

    make some function that increase or decrease counter in UI using useState hook

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,30 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [count, setCount] = useState(0)
+  let [counter,setCounter] = useState(0) 
+  // let counter = 0;
+  function addValue(){
+    if(counter+1>20){
+      return;
+    }
+    counter = counter+1;
+    setCounter(counter)
+    console.log(counter);
+  }
+  const remvoeValue = () =>{
+    if(counter-1<0){
+      return;
+    }
+    counter = counter-1;
+    setCounter(counter)
+    console.log(counter);
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>chai aur react</h1>
+      <h2>counter value: {counter}</h2>
+      <button onClick={addValue}>Add value {counter}</button>
+      <br /><br />
+      <button onClick={remvoeValue}>remove value {counter}</button>
     </>
   )
 }
