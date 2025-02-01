1e27c206e81c7f1f7c29db746d0086975fb01cd0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-01 14:48:23 +0530

    complete a project where the bacgroud color changed when you click the color button

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,34 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("white");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-full h-screen" style={{
+      backgroundColor:color
+    }}>
+      <button style={{backgroundColor:"green"}} onClick={()=>setColor("green")}>green</button>
+      <button style={{backgroundColor:"blue"}} onClick={()=>setColor("blue")}>blue</button>
+      <button style={{backgroundColor:"red"}} onClick={()=>{setColor("red")}}>red </button>
+      <button style={{backgroundColor:"olive"}} onClick={()=>{setColor("olive")}}>olive </button>
+      <button style={{backgroundColor:"pink"}} onClick={()=>{setColor("pink")}}>pink </button>
+      <button style={{backgroundColor:"violet"}} onClick={()=>{setColor("violet")}}>violet</button>
+    </div>
   )
 }
 
