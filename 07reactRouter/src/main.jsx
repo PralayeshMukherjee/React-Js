6e250595f0d225e9c82f659273e1260e293a7e73
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-07 00:35:24 +0530

    make contact page using router and also use the new routing fether

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,26 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './Componenet/Home/Home.jsx'
 import About from './Componenet/About/About.jsx'
+import Contact from './Componenet/Contact/Contact.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout/>,
-    children: [
-      {path: "home",
-        element: <Home/>
-      },
-      {
-        path: "about",
-        element: <About/>
-      }
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {path: "home",
+//         element: <Home/>
+//       },
+//       {
+//         path: "about",
+//         element: <About/>
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='home' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
