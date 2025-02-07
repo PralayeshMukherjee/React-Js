d4e1fb8874ae4bc35dc2e8a38e7b4abad9a667ca
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-07 21:42:07 +0530

    use outlet to dynamically render the content in nested router

--- myWebsite/src/Router.jsx
+++ myWebsite/src/Router.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PagesHeader from './Component/Header/PagesHeader'
 import PagesFooter from './Component/Footer/PagesFooter'
+import { Outlet } from 'react-router-dom'
 
 function Router() {
   return (
     <>
         <PagesHeader/>
+        <Outlet/>
         <PagesFooter/>
     </>
   )
