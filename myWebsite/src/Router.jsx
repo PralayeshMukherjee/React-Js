e3ac3e2aaac03daeabb20d56133b90537efee963
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-07 19:58:52 +0530

    make router file you can call it Layout file also

---
+++ myWebsite/src/Router.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PagesHeader from './Component/PagesHeader'
+
+function Router() {
+  return (
+    <>
+        <PagesHeader/>
+    </>
+  )
+}
+
+export default Router
