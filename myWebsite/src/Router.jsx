cbc3de89ff086b7404f2c50f11c68d6fd4677015
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-07 21:06:59 +0530

    add the pagesfooter tag in router function

--- myWebsite/src/Router.jsx
+++ myWebsite/src/Router.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PagesHeader from './Component/Header/PagesHeader'
+import PagesFooter from './Component/Footer/PagesFooter'
 
 function Router() {
   return (
     <>
         <PagesHeader/>
+        <PagesFooter/>
     </>
   )
 }
