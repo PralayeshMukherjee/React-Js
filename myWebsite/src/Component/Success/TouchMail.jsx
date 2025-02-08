ada936df611fcce1092ad7da4da775b31b7050eb
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-09 00:21:47 +0530

    update the success page

--- myWebsite/src/Component/Success/TouchMail.jsx
+++ myWebsite/src/Component/Success/TouchMail.jsx
@@ -3,11 +3,14 @@ import {Link} from 'react-router-dom'
 
 function TouchMail() {
   return (
-    <div class="container mx-auto p-6 bg-white shadow-lg rounded-lg max-w-md text-center">
-        <h1 class="text-green-500 text-2xl font-semibold">Mail Successfully Send!</h1>
-        <p class="text-gray-700 text-base">Your all valuable message send to our server. Thank you for you time.</p>
-        <Link to="/home" class="mt-6 inline-block text-white bg-green-500 py-2 px-6 rounded-lg hover:bg-green-600 transition-colors">Go to Home Page</Link>
+    <div class="flex items-center justify-center bg-violet-100">
+        <div class="container mx-auto p-6 bg-violet-100 shadow-lg rounded-lg max-w-md text-center">
+            <h1 class="text-green-500 text-2xl font-semibold">Mail Successfully Sent!</h1>
+            <p class="text-gray-700 text-base break-words">Your valuable message has been sent to our server. Thank you for your time.</p>
+            <Link to="/home" class="mt-6 inline-block text-white bg-green-500 py-2 px-6 rounded-lg hover:bg-green-600 transition-colors">Go to Home Page</Link>
+        </div>
     </div>
+
 
   )
 }
--- myWebsite/src/main.jsx
+++ myWebsite/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom'
-import {Router,Home,About,Contact} from './Component/index.js'
+import {Router,Home,About,Contact, TouchMail} from './Component/index.js'
 
 const MyRouter = createBrowserRouter(
   createRoutesFromElements(
@@ -11,6 +11,7 @@ const MyRouter = createBrowserRouter(
         <Route path='home' element={<Home/>}/>
         <Route path='about' element={<About/>}/>
         <Route path='contact' element={<Contact/>}/>
+        <Route path='TouchMail' element={<TouchMail/>} />
       </Route>
   )
 );
