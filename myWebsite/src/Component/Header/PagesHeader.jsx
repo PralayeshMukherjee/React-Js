5bc67ea5e45b95999f7952196ac124abd29442dd
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-08 20:59:06 +0530

    modify the css property

--- myWebsite/src/Component/Header/PagesHeader.jsx
+++ myWebsite/src/Component/Header/PagesHeader.jsx
@@ -17,8 +17,7 @@ export default function PagesHeader() {
                     <div className="flex items-center lg:order-2">
                         <Link
                             to="#"
-                            className="text-white-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                            style={{color:"white",font:"55px"}}
+                            className="text-white hover:bg-gray-50 hover:text-black focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
                         </Link>
@@ -37,33 +36,24 @@ export default function PagesHeader() {
                             <li>
                                 <NavLink
                                 to={"/home"}
-                                    className={(isActive) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-gray-700" : "text-orange-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                    style={{color:"white"}}
+                                    className={`text-white hover:text-orange-400`}
                                 >
                                     Home
                                 </NavLink>
                             </li>
                             <li>
                                 <NavLink
-                                to={"/About"}
-                                    className={(isActive) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-gray-700" : "text-orange-700"} border-b   border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                    style={{color:"white"}}
+                                to={"/about"}
+                                    className={`text-white hover:text-orange-400`}
                                 >
                                     About
                                 </NavLink>
                             </li>
                             <li>
                                 <NavLink
-                                to={"/Contact"}
-                                    className={(isActive) =>
-                                        `block py-2 pr-4 pl-3   duration-200 $    {isActive ?     "text-gray-700" :   "text-orange-700"}    border-b border-gray-100   hover:bg-gray-50  lg:hover:bg-transparent  lg:border-0  hover:text-orange-700    lg:p-0`
-                                    }
-                                    style={{color:"white"}}
-                                >
+                                    to={"/contact"}
+                                        className={`text-white hover:text-orange-400`}
+                                    >
                                     Contact
                                 </NavLink>
                             </li>
--- myWebsite/src/Component/Home/Home.jsx
+++ myWebsite/src/Component/Home/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
   return (
     <div className="hero w-full min-h-screen bg-violet-100">
         <section className="firstSection flex justify-around p-0 m-0 items-center my-[130px] text-black" style={{margin:"auto"}}>
-            <div className="leftSection w-[30%] text-2xl my-8">
+            <div className="leftSection w-[30%] text-3xl my-8">
                 <div>Welcome to the Library</div>
                 <div>Here you can find:-</div>
                 <span id="element" className="text-[#aa6be4]" ref={typedMsg}></span>
