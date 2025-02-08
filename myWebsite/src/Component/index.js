5874fa4cf1271d4a0271401f9559881683b90fa6
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-09 00:08:16 +0530

    import the touch mail method in index file

--- myWebsite/src/Component/index.js
+++ myWebsite/src/Component/index.js
@@ -2,5 +2,6 @@ import Home from './Home/Home.jsx'
 import About from './About/About.jsx'
 import Contact from './Contact/Contact.jsx'
 import Router from '../Router.jsx'
+import TouchMail from './Success/TouchMail.jsx'
 
-export {Router,Home,About,Contact};
+export {Router,Home,About,Contact,TouchMail};
